17cd8959785fe25d208de3265a8a6198f7cec4c5
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-22 02:42:55 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,7 +9,7 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[5.2]
-  def class
+  def create
       create_table :costumes do |t|
         t.string :name
         t.float :price
