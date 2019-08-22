5f39456a23f35dae688cf5d49e2bbcdfecc328a3
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-22 03:18:35 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
+ActiveRecord::Schema.define(version: 2) do
+
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
