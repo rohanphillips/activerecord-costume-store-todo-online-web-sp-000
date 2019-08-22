4c2d633eb5680832aeb82a7e0cb8a2cf418d85d8
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-22 03:15:12 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,7 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def create
+      binding.pry
       create_table :costume_stores do |t|
         t.string :name
         # t.string :location
