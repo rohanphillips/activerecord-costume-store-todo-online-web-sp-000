d44ec65f6f76dee12bebff028e9ee241773f901a
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-22 02:43:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
-  def class
+  def create
       create_table :haunted_houses do |t|
         t.string :name
         t.string :location
