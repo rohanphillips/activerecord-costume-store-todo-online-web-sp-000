1df68a12cc66c705d04aee1bbf6634832b3911af
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-22 03:12:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,8 +3,12 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def create
       create_table :costume_stores do |t|
         t.string :name
-        t.string :location
-
+        # t.string :location
+        # t.integer :costume_inventory
+        # t.integer :employees
+        # t.boolean :in_business
+        # t.datetime :opening_time
+        # t.datetime :closing_time
       end
   end
 end
